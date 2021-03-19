1d505b327d023ca2423efe46b9c52585a39b8050
Endzhe Galieva <endzhe91@gmail.com>
2021-03-19 21:58:01 +0300

    added content

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'storyblok-blog',
+    title: "Storyblok + Nuxt = <3",
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: "An awesone blog about tech stuff, built with Nuxt and Storyblok"}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400,700&'}
     ]
   },
 
