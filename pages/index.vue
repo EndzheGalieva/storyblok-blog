6a9df7f45621560c7802bc997916a461b1a58d9c
Endzhe Galieva <endzhe91@gmail.com>
2021-03-21 01:00:02 +0300

    added posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,58 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        storyblok-blog
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div id="posts">
+    <PostPreview
+    v-for="post in posts"
+    :key="post.id"
+    :title="post.title"
+    :excerpt="post.previewText"
+    :thumbnailImage="post.thumbnailUrl"
+    :id="post.id" />
   </div>
 </template>
 
 <script>
-export default {}
+import PostPreview from '~/components/Blog/PostPreview'
+
+export default {
+  components: { PostPreview },
+  component: {
+    PostPreview
+  },
+  data() {
+   return {
+     posts: [
+       {
+       title: 'A New Beginning',
+       previewText: 'This will be awesome, don\'t miss it!',
+       thumbnailUrl:
+         'https://media.healthyfood.com/wp-content/uploads/2016/09/Courgette-and-pea-pasta-with-ricotta-cheese-1024x657.jpg',
+       id: 'a-new-beginning'
+       },
+       {
+         title: 'A Second Beginning',
+         previewText: 'This will be awesome, don\'t miss it!',
+         thumbnailUrl:
+           'https://media.healthyfood.com/wp-content/uploads/2016/09/Courgette-and-pea-pasta-with-ricotta-cheese-1024x657.jpg',
+         id: 'a-second-beginning'
+       }
+     ]
+   };
+ }
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style scoped>
+#posts {
+  padding-top: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  flex-direction: column;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
 }
 </style>
